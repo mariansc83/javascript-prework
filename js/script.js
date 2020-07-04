9362b71d3a3711a56e3f5dd0b877db1d3a244b65
Marian-Rozycki <67416143+Marian-Rozycki@users.noreply.github.com>
2020-07-04 20:39:39 +0200

    Change quantity of else...if in code.

--- js/script.js
+++ js/script.js
@@ -32,26 +32,12 @@ printMessage('Twój ruch to: ' + playerMove);
 
 if (computerMove == 'kamień' && playerMove == 'papier') {
     printMessage('Ty wygrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'kamień') {
-    printMessage('Niestety przegrywasz');
-} else if (computerMove == 'kamień' && playerMove == 'kamień') {
-    printMessage('Remis!');
-} else if (computerMove == 'papier' && playerMove == 'papier') {
-    printMessage('Remis!');
-} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-    printMessage('Niestety przegrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-    printMessage('Ty wygrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-    printMessage('Remis!');
-} else if (computerMove == 'nożyce' && playerMove == 'papier') {
-    printMessage('Niestety przegrywasz!');
 } else if (computerMove == 'papier' && playerMove == 'nożyce') {
     printMessage('Ty wygrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'nieznany ruch') {
-    printMessage('Nieznany ruch gracza, wybierz pomiedzy 1, 2 lub 3.');
-} else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
-    printMessage('Nieznany ruch gracza, wybierz pomiedzy 1, 2 lub 3.');
-} else if (computerMove == 'papier' && playerMove == 'nieznany ruch') {
-    printMessage('Nieznany ruch gracza, wybierz pomiedzy 1, 2 lub 3.');
+} else if (computerMove == playerMove) {
+    printMessage('Remis!');
+} else if (playerMove == 'nieznany ruch') {
+    printMessage('Nieznany ruch! Wybierz pomiędzy 1, 2 lub 3.');
+} else {
+    printMessage('Niestety przegrywasz!');
 }
