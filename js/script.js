327383b201f70bd759163a871d03a10a1ae3627d
Marian-Rozycki <67416143+Marian-Rozycki@users.noreply.github.com>
2020-07-05 12:02:41 +0200

    Add functions doesn't work correctly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch';
+    }
+}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
 if (randomNumber == '1') {
     computerMove = 'kamień';
 } else if (randomNumber == '2') {
@@ -11,26 +20,33 @@ if (randomNumber == '1') {
 } else if (randomNumber == '3') {
     computerMove = 'nożyce';
 }
-
-printMessage('Mój ruch to: ' + computerMove);
-
+printMessage('Mój ruch to: ' + getMoveName(randomNumber));
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
+let playerMove = getMoveName(randomNumber);
+printMessage('Twój ruch to: ' + getMoveName(randomNumber));
 
-let playerMove = 'nieznany ruch';
+function displayResult(argComputerMove, argPlayerMove) {
+    // printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
+    if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+        printMessage('Ty wygrywasz!');
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+        printMessage('Ty wygrywasz!');
+    } else if (argComputerMove == argPlayerMove) {
+        printMessage('Remis!');
+    } else if (argPlayerMove == 'nieznany ruch') {
+        printMessage('Nieznany ruch! Wybierz pomiędzy 1, 2 lub 3.');
+    } else {
+        printMessage('Niestety przegrywasz!');
+    }
+
 }
+displayResult();
 
-printMessage('Twój ruch to: ' + playerMove);
+
 // Zmienione warunki
-if (computerMove == 'kamień' && playerMove == 'papier') {
+/* if (computerMove == 'kamień' && playerMove == 'papier') {
     printMessage('Ty wygrywasz!');
 } else if (computerMove == 'papier' && playerMove == 'nożyce') {
     printMessage('Ty wygrywasz!');
@@ -40,4 +56,4 @@ if (computerMove == 'kamień' && playerMove == 'papier') {
     printMessage('Nieznany ruch! Wybierz pomiędzy 1, 2 lub 3.');
 } else {
     printMessage('Niestety przegrywasz!');
-}
+} */
