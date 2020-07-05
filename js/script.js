14272af3a608cedb79b8ac552b2fa2ea2becc2f6
Marian-Rozycki <67416143+Marian-Rozycki@users.noreply.github.com>
2020-07-05 15:30:55 +0200

    Change functions arguments. printMessage doesn't work.

--- js/script.js
+++ js/script.js
@@ -10,24 +10,19 @@ function getMoveName(argMoveId) {
         return 'nieznany ruch';
     }
 }
+
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = getMoveName(randomNumber);
-if (randomNumber == '1') {
-    computerMove = 'kamień';
-} else if (randomNumber == '2') {
-    computerMove = 'papier';
-} else if (randomNumber == '3') {
-    computerMove = 'nożyce';
-}
-printMessage('Mój ruch to: ' + getMoveName(randomNumber));
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
-let playerMove = getMoveName(randomNumber);
-printMessage('Twój ruch to: ' + getMoveName(randomNumber));
+let playerMove = getMoveName(playerInput);
+printMessage('Twój ruch to: ' + playerMove);
 
 function displayResult(argComputerMove, argPlayerMove) {
-    // printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
     if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
         printMessage('Ty wygrywasz!');
@@ -40,20 +35,5 @@ function displayResult(argComputerMove, argPlayerMove) {
     } else {
         printMessage('Niestety przegrywasz!');
     }
-
+    displayResult(argComputerMove, argPlayerMove);
 }
-displayResult();
-
-
-// Zmienione warunki
-/* if (computerMove == 'kamień' && playerMove == 'papier') {
-    printMessage('Ty wygrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-    printMessage('Ty wygrywasz!');
-} else if (computerMove == playerMove) {
-    printMessage('Remis!');
-} else if (playerMove == 'nieznany ruch') {
-    printMessage('Nieznany ruch! Wybierz pomiędzy 1, 2 lub 3.');
-} else {
-    printMessage('Niestety przegrywasz!');
-} */
